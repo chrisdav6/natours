0cb3244080e570ba771c2d821343a60a31020bd8
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-20 11:43:34 -0400

    Lesson 84 - Creating a simple tour schema and model

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ mongoose.connect(DB, {
   useUnifiedTopology: true
 }).then(() => console.log('Connected to DB'));
 
+//Schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name!'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    defualt: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price!']
+  }
+});
+
+//Model
+const Tour = mongoose.model('Tour', tourSchema);
+
 //Start Server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
