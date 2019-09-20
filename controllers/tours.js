41ca49c22f223fc6d02b3ae7b11808f27dea88de
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-20 15:13:13 -0400

    Lesson 90 - Updating documents

--- controllers/tours.js
+++ controllers/tours.js
@@ -3,6 +3,7 @@ const Tour = require('../models/tourModel');
 const getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find();
+
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -21,6 +22,7 @@ const getAllTours = async (req, res) => {
 const getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
+
     res.status(200).json({
       status: 'success',
       data: {
@@ -38,6 +40,7 @@ const getTour = async (req, res) => {
 const createTour = async (req, res) => {
   try {
     const newTour = await Tour.create(req.body);
+
     res.status(201).json({
       status: 'success',
       data: {
@@ -52,17 +55,22 @@ const createTour = async (req, res) => {
   }
 }
 
-const updateTour = (req, res) => {
-  //Get Tour
-  const tour = tours.find(el => el.id == req.params.id);
+const updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
 
-  res.status(200).json({
-    status: 'success',
-    updated: true,
-    data: {
-      tour
-    }
-  })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    })
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
 }
 
 const deleteTour = (req, res) => {
