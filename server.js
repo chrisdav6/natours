140a46017fed73ff7e31fd4c71ab143a08ae6292
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-20 11:03:28 -0400

    Lesson 82 - Creating Atlas DB and installing Mongoose

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 require('dotenv').config({ path: './config.env' });
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const port = process.env.port || 3000;
 
+//Connect to DB
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DBPASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => console.log('Connected to DB'));
+
+//Start Server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
