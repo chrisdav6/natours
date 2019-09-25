39d6b1f2fd2837e1fc5940f92690efe3340b2d92
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-25 14:31:12 -0400

    Lesson 101 - Aggregation Pipeline: Matching and Grouping

--- controllers/tours.js
+++ controllers/tours.js
@@ -75,7 +75,7 @@ const updateTour = async (req, res) => {
       data: {
         tour
       }
-    })
+    });
   } catch (err) {
     res.status(404).json({
       status: 'fail',
@@ -100,7 +100,45 @@ const deleteTour = async (req, res) => {
   }
 }
 
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRating: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats
+      }
+    });
+
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+}
+
 module.exports = {
+  getTourStats,
   aliasTopTours,
   getAllTours,
   getTour,
