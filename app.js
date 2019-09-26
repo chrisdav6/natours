f577e756cdd792202e73ad42ca1587a52966206f
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-26 14:16:51 -0400

    Lesson 114 - Better Errors and Refactoring

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tours');
 const userRouter = require('./routes/users');
@@ -29,27 +31,15 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // res.status(404).json({
-  //   status: 'fail',
-  //   message: `Can't find ${req.originalUrl}`
-  // });
-  // next();
-  const err = new Error('Test Error');
-  err.status = 'fail';
-  err.statusCode = 404;
 
-  next(err);
+  // const err = new Error('Test Error');
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError('Cant find URL', 404));
 });
 
 //Error Handling
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
