71e1a79860bb0883ec18899cb992b5002bffa52e
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-26 11:38:30 -0400

    Lesson 108 - Data Validation - Custom Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 //Schema
 const tourSchema = new mongoose.Schema({
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema({
     trim: true,
     maxlength: [40, 'A tour name must have less or equal than 40 characters!'],
     minlength: [10, 'A tour name must have more or equal than 10 characters!']
+    // validate: [validator.isAlpha, 'Tour name cannot contain numbers!']
   },
   slug: {
     type: String
@@ -45,7 +47,14 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a price!']
   },
   priceDiscount: {
-    type: Number
+    type: Number,
+    validate: {
+      validator: function (val) {
+        // This will only point to current doc on NEW document creation - will not work with UPDATE
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) cannot be higher than regular price'
+    }
   },
   summary: {
     type: String,
