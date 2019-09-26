4b65b9eca86cc7a47610f2c6f59283674a490b7a
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-26 14:02:06 -0400

    Lesson 113 - Global Error Handling Middleware

--- app.js
+++ app.js
@@ -29,11 +29,27 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`
-  });
-  next();
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl}`
+  // });
+  // next();
+  const err = new Error('Test Error');
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+//Error Handling
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  })
 });
 
 module.exports = app;
