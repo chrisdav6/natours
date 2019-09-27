f64e090337839d828a9972c9d6e9a272ce934012
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-27 14:59:04 -0400

    Lesson 125 - Managing Passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //Schema
 const userSchema = new mongoose.Schema({
@@ -24,8 +25,28 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm password!']
+    required: [true, 'Please confirm password!'],
+    validate: {
+      validator: function (el) {
+        // This will only work with NEW user creation .create() and .save() - will not work with UPDATE
+        return el === this.password;
+      },
+      message: `Passwords do not match!`
+    }
   }
+});
+
+//Encrypt the password
+userSchema.pre('save', async function (next) {
+  //Only run this function if password is modified
+  if (!this.isModified('password')) return next;
+
+  //Hash the password
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next()
 });
 
 //Model
