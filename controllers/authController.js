92e9a27c87b282dc39aa89611aa0880b3e06b6ac
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2019-09-30 08:09:25 -0400

    Lesson 128 - Signing Up Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,25 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 const signup = catchAsync(async (req, res, next) => {
   //Create new user
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
+
+  //Log new user in
+  const token = jwt.sign({ id: newUser._id }, process.env.JWTSECRET,
+    { expiresIn: process.env.JWTEXPIRESIN }
+  );
 
   //Send Response
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
